b675fb2ef603061cf35b0cb3cdc1781db5bf6a56
JustCode09 <Professionalacc011@gmail.com>
2026-05-13 09:14:27 +0545

    Initial commit – MyFirstWebAPI with StudentController

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstWebAPI.Controllers   
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "Hello Student API";
+        }
+    }
+}
