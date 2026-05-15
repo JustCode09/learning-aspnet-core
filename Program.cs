5cc49efd25daf4e14874bb46f209cea48aff47a9
JustCode09 <Professionalacc011@gmail.com>
2026-05-15 13:23:27 +0545

    Add Classes & Objects summary notes for Topic 6

--- Program.cs
+++ Program.cs
@@ -1,23 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+// Creating student objects
+using MyFirstWebAPI;
 
-// Add services to the container.
+Student student1 = new Student();
+student1.Id = 1;
+student1.Name = "Ram";
+student1.Age = 20;
+student1.GPA = 3.5;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Student student2 = new Student();
+student2.Id = 2;
+student2.Name = "Sita";
+student2.Age = 22;
+student2.GPA = 3.8;
 
-var app = builder.Build();
+// Call the method
+student1.Introduce();
+student2.Introduce();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+// Print individual properties
+Console.WriteLine($"Student 1 ID: {student1.Id}");
+Console.WriteLine($"Student 2 Name: {student2.Name}");
---
+++ Student.cs
@@ -0,0 +1,15 @@
+﻿namespace MyFirstWebAPI
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double GPA { get; set; }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hi, I am {Name}, Age: {Age}, GPA: {GPA}");
+        }
+    }
+}
