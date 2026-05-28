3a3e36bda6abcb54887c6134960936585fa37953
JustCode09 <Professionalacc011@gmail.com>
2026-05-28 09:50:43 +0545

    crud

--- Services/StudentService.cs
+++ Services/StudentService.cs
@@ -1,56 +1,64 @@
-﻿using MyFirstWebAPI.Models;
+﻿using MyFirstWebAPI.Data;
+using MyFirstWebAPI.Models;
 
 namespace MyFirstWebAPI.Services
 {
     public class StudentService : IStudentService
     {
-        private static List<Student> students = new List<Student>
-        {
-            new Student { Id = 1, Name = "Ram", Age = 20, GPA = 3.9 },
-            new Student { Id = 2, Name = "Sita", Age = 22, GPA = 3.4 },
-            new Student { Id = 3, Name = "Hari", Age = 21, GPA = 3.7 }
-        };
+        // Door to database
+        private readonly AppDbContext _context;
 
+        // .NET gives us database automatically
+        public StudentService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // CREATE
         public Student AddStudent(Student student)
         {
-            student.Id = students.Count + 1;
-            students.Add(student);
+            _context.Students.Add(student);
+            _context.SaveChanges();
             return student;
         }
 
+        // READ ALL
         public List<Student> GetAllStudents()
         {
-            return students;
+            return _context.Students.ToList();
         }
 
+        // READ ONE
         public Student GetStudentById(int id)
         {
-            return students.FirstOrDefault(s => s.Id == id);
+            return _context.Students
+                   .FirstOrDefault(s => s.Id == id);
         }
 
+        // UPDATE
         public Student UpdateStudent(int id, Student updatedStudent)
         {
-            var student = students.FirstOrDefault(s => s.Id == id);
-
-            if (student == null)
-                return null;
+            var student = _context.Students
+                          .FirstOrDefault(s => s.Id == id);
+            if (student == null) return null;
 
             student.Name = updatedStudent.Name;
             student.Age = updatedStudent.Age;
             student.GPA = updatedStudent.GPA;
 
+            _context.SaveChanges();
             return student;
         }
 
+        // DELETE
         public bool DeleteStudent(int id)
         {
-            var student = students.FirstOrDefault(s => s.Id == id);
+            var student = _context.Students
+                          .FirstOrDefault(s => s.Id == id);
+            if (student == null) return false;
 
-            if (student == null)
-                return false;
-
-            students.Remove(student);
-
+            _context.Students.Remove(student);
+            _context.SaveChanges();
             return true;
         }
     }
